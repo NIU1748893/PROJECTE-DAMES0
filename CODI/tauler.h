bb96fd1f6fe9368fd50c7057d7eddff235abd011
Victor Niccolai Periago <1748893@uab.cat>
2025-05-01 12:00:19 +0200

    comiit 2

---
+++ CODI/tauler.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef TAULER_H
+#define TAULER_H
+
+#include "Fitxa.h"
+#include "Posicio.h"
+#include <string>
+using namespace std;
+
+const int N_FILES = 8;
+const int N_COLUMNES = 8;
+
+class Tauler {
+private:
+    Fitxa m_tauler[N_FILES][N_COLUMNES];
+
+public:
+    void inicialitza(const std::string& nomFitxer);
+    void actualitzaMovimentsValids();
+    void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
+    bool mouFitxa(const Posicio& origen, const Posicio& desti);
+    string toString() const;
+};
+
+#endif
